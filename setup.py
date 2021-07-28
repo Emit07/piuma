c98c0d556d0036cfc0893f964c3642667a5c1fb0
Ale De Leo <adv.deleo@gmail.com>
2021-07-29 01:43:13 +0200

    PyPi: fixed pypi description rst error

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
-LONG_DESCRIPTION = """Piuma is an no-headache ultra-lightweight local document 
+LONG_DESCRIPTION = """
+Piuma
+=====
+
+Piuma is an no-headache ultra-lightweight local document 
 database written purely in python. With just over 50 lines (54 lines to be 
 precise) of active code and a source file that is 3.39kb piuma is designed without 
 just the **absoulute bare** essentials in mind. In the core version of Piuma there 
@@ -12,29 +16,24 @@ should really only be used for small projects and testing.
 Why you *should* use Piuma?
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-- **Light and Small:** Piuma is written in 54 lines of code and can be sub 
-millisecond fast because of its memory storage
+- **Light and Small:** Piuma is written in 54 lines of code and can be sub millisecond fast because of its memory storage
 - **Easy to Use:** Piuma runs out of the box and is easy to use and learn
-- **Experimenting:** If you are trying something new and you need a fast 
-database that *just works* then Piuma is a good choice
+- **Experimenting:** If you are trying something new and you need a fast database that *just works* then Piuma is a good choice
 
 
 Why you *should not* use Piuma?
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-- **Big Database:** Piuma was not designed to be used as a big database; Piuma 
-only runs in *only* memory (for now) so the limit of how big it could be comes 
-much quicker than other database
-- **ACID Guarantees:** Because of Piumas small nature it does not have room 
-for ACID Guarantees
-- **Query Language:** Piuma does not have a query language (allthough I plan 
-to add one in a submodule in the future)
+- **Big Database:** Piuma was not designed to be used as a big database; Piuma only runs in *only* memory (for now) so the limit of how big it could be comes much quicker than other database
+- **ACID Guarantees:** Because of Piumas small nature it does not have room for ACID Guarantees
+- **Query Language:** Piuma does not have a query language (allthough I plan to add one in a submodule in the future)
 
 Why is it called Piuma?
 ~~~~~~~~~~~~~~~~~~~~~~~
 
 Piuma is the Italian word for feather and (from what I feel) the 
-word feather can convay lightness. Also I am a little proud of the logo I made :]"""
+word feather can convay lightness. Also I am a little proud of the logo I made :]
+"""
 
 
 setup(
