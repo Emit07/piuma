1e6ffa30c33af0c0c2840664200443f027b69dfc
emit07 <emit07@protonmail.com>
2026-05-29 21:12:58 -0700

    modified long description

--- setup.py
+++ setup.py
@@ -4,50 +4,44 @@ LONG_DESCRIPTION = """
 Piuma
 =====
 
-Piuma is an no-headache ultra-lightweight local document database written
-purely in python. Piuma is designed without just the **absoulute bare**
-essentials in mind. In the core version of Piuma there is no query language or
-any advanced search/modify functions. In its current version Piuma only stores
-data in memory, I plan to add local storage subpackage hopefully in the near
-future. This database should not be taken too seriously and should really only
-be used for small projects and testing.
+Piuma is a lightweight Python library that provides a simple
+document-oriented interface layer with pluggable storage backends. Piuma
+is designed around a minimal and flexible core that makes it easy to:
+
+-  work with structured and document-like data
+-  swap or extend storage implementations
+-  build custom database behaviors
+
+Out of the box, Piuma can be used as a small memory or file backed
+document store. However, its primary design goal is to serve as a
+foundation for building more complex or specialized database systems.
+Piuma has three core structural principles: minimal design,
+extensibility, compositional architecture.
 
 Quick Links
 -----------
-- Github: https://github.com/emit07/piuma
-- Documentation: https://piuma.readthedocs.io/
 
-Why you *should* use Piuma?
-~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-  Github: https://github.com/emit07/piuma
+-  Documentation: https://piuma.readthedocs.io/
+-  PyPi: https://pypi.org/project/piuma
 
-- **Light and Small:** Piuma's core is written with one very small file and can
-    be sub millisecond fast because of its memory storage
-- **Easy to Use:** Piuma runs out of the box and is easy to use and learn.
-    Piuma also requires no dependencies or external servers.
-- **Experimenting:** If you are trying something new and you need a fast
-    database that just works while testing then Piuma is a good choice.
-- **No Dependencies:** Piuma is written with no external dependecies.
+Use Cases
+---------
 
-Why you *should not* use Piuma?
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Piuma works well in situations where you want a lightweight and
+embeddable data layer.
 
-- **Big Database:** Piuma was not designed to be used as a big database; Piuma
-    only runs *only* in memory (for now) so the limit of how big it could be
-    comes much quicker than other database
-- **ACID Guarantees:** Because of Piumas small nature it does not have room for
-    ACID_ Guarantees
-- **Query Language:** Piuma does not have a query language (though I currently
-    working on a query submodule)
-- **Storage:** While Piuma supports interchangeable storage, it only has memory
-    storage built in
+-  Building prototypes or experimental data systems
+-  Adding simple document storage to Python applications
+-  Creating a custom persistence layer without adopting a full database
+-  Flexibility in rapid development
+-  Testing environments
 
 Why is it called Piuma?
-~~~~~~~~~~~~~~~~~~~~~~~
+-----------------------
 
-Piuma is the Italian word for feather and (from what I feel) the word feather
-can convay lightness.
-
-.. _ACID: https://en.wikipedia.org/wiki/ACID
+Piuma (‹più·ma›) is the Italian word for feather, which reflects the
+design goals.
 """
 
 setup(
@@ -56,7 +50,7 @@ setup(
     license="gpl-3.0",
     author="Alessandro De Leo",
     author_email="emit07@protonmail.com",
-    description="An ultra-lighweight document oriented database",
+    description="A lighweight document oriented database interface written in python",
     long_description_content_type="text/x-rst",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
